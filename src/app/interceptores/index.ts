6233ea755242c1d4ad44f4354d289f24b871fc2f
Michael Vargas <vacomi.corp@gmail.com>
2023-01-18 20:47:45 -0500

    tarea finalizada

--- src/app/interceptores/index.ts
+++ src/app/interceptores/index.ts
@@ -1,8 +1,6 @@
 import { HTTP_INTERCEPTORS } from "@angular/common/http";
 import { MiInterceptorService } from "./mi-interceptor.service";
 
-// Esto lo creamos en este archivo para seguir agregando
-// Interceptores
 
 export const httpInterceptorProviders = [
     { provide: HTTP_INTERCEPTORS, useClass: MiInterceptorService , multi: true }
